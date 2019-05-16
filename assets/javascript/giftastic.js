dc5672e994a06029a89e876120b5baeb76098c75
stbrooks <stephanie.brooks@outlook.com>
2019-05-16 16:31:33 -0700

    updated

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,9 +1,10 @@
 //array for topic, my topic is also animals (how creative)
+$(document).ready(function () {
 var animals = ["Dog", "Cat", "Tiger", "Monkey", "Sloth", "Giraffe", "Bird", "Donkey", "Raccoon", "Panda"];
 
 function alertAnimalName() {
 
-  }
+  };
 
 //need a function to render buttons. Homework suggests using a loop to go through the array
 function renderButtons (){
@@ -23,51 +24,56 @@ for (var i=0; i<animals.length; i++){
     //append buttons
     $("#animal-Buttons").append(ab);
     }
-}
+};
 
 // This function handles events where one button is clicked
-      $("#add-animal").on("click", function(event) {
-        event.preventDefault();
+   $("#add-animal").on("click", function(event) {
+       console.log(test);
+   event.preventDefault();
 
         // This line grabs the input from the textbox
-        var movie = $("#animal-input").val().trim();
+   var ani = $("#animal-input").val().trim();
 
         // The movie from the textbox is then added to our array
-        animals.push(movie);
+        animals.push(ani);
 
         // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-
-        $(document).on("click", ".animal", alertAnimalName);
+     renderButtons();
+    });
+//click function for animal class buttons
+      $(document).on("click", ".animal", alertAnimalName);
 
         // Calling the renderButtons function to display the intial buttons
-        renderButtons(); 
-      });
+      renderButtons(); 
+    
 
-      $("#animal-buttons").on("click", function() {
+      $("#animal-Buttons").on("click", function() {
 
-        //creates a variable for the image search
-        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=FT79lMtvzGCwg7NZYGS8Qd1bkn19VQ51&tag=" + data.name;
+       // creates a variable for the image search
+      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=FT79lMtvzGCwg7NZYGS8Qd1bkn19VQ51&tag=" + ;
   
-        //gets image from the api
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
+   // gets image from the api
+       $.ajax({
+     url: queryURL,
+    method: "GET"
+      })
   
-        //calls out 
-          .then(function(response) {
+    //calls out 
+       .then(function(response) {
   
           //variable for the image
-            var imageUrl = response.data.image_original_url;
+       var imageUrl = response.data.image_original_url;
   
             //create image element to drop animal images into
-            var animalImage = $("<img>");
+          var animalImage = $("<img>");
   
             //replace image src with url for gif
-            animalImage.attr("src", imageUrl);
-            //alt image description
-            animalImage.attr("alt", "Animal image");
+        animalImage.attr("src", imageUrl);
+        //alt image description
+        animalImage.attr("alt", "Animal image");
   
             //append image of animal selected
-            $("#images").append(animalImage);
+          $("#images").append(animalImage);
+       })
+    })
+    })
